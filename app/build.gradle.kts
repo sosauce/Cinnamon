fe89ea18ebdee68b1228c2f70654efc518604114
sosauce <98750531+sosauce@users.noreply.github.com>
2026-09-21 23:22:45 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "com.sosauce.cinnamon"
         minSdk = 26
         targetSdk = 37
-        versionCode = 4
-        versionName = "1.0.4"
+        versionCode = 5
+        versionName = "1.1.0"
 
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
